4c4579d71c36ec0c115ef3fee2a2ae86aeb77c2c
赵雪丽 <qq_4314175511@noreply.gitcode.com>
2026-08-07 11:56:32 +0800

    about.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    build: {
+        format: 'file', // 打包成单个文件
+    },
+    trailingSlash: 'never',
+});
